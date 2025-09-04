5e02a1f982838c0989f50a089ca71855b71b9a7d
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-09-04 08:23:17 +0200

    Lesson 166

---
+++ 09-security/flightticket_controller.go
@@ -0,0 +1,9 @@
+package flightticket
+
+var controllerKind = apps.SchemeGroupVersion.WithKind("FlightTicket");
+
+// Run begins watching and syncing
+func (dc *FlightTicketController) Run(workers int, stopCh <-chan struct{})
+
+// Call BookFlightAPIReplicaSet
+func (dc *FlightTicketController) callBookFlightAPI(obj interface{})
